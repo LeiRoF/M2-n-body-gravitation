89cfe0127eb0816af76373adb8f4e1debfd665ea
Leirof <vince.lrf@gmail.com>
2022-12-03 17:04:47 +0100

    Better input parameters

--- config.f90
+++ config.f90
@@ -1,7 +1,7 @@
 module config
 
-    integer, parameter :: N = 4000 ! number of bodies
-    integer, parameter :: steps = 10 ! simulation time in steps
+    integer, parameter :: N = 1000 ! number of bodies
+    integer, parameter :: steps = 100 ! simulation time in steps
     real, parameter    :: dt = 0.1 ! time step in seconds
     logical            :: new_initial_conditions = .true. ! set to .true. to generate new initial conditions
     logical            :: verbose = .false.
