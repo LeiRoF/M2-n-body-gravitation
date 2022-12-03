cc2eeb54b0112fced4bed34da6d5c4e98fb8dbf6
Leirof <vince.lrf@gmail.com>
2022-12-03 15:15:14 +0100

    New initial condition on true by default

--- config.f90
+++ config.f90
@@ -3,7 +3,7 @@ module config
     integer, parameter :: N = 4000 ! number of bodies
     integer, parameter :: steps = 10 ! simulation time in steps
     real, parameter    :: dt = 0.1 ! time step in seconds
-    logical            :: new_initial_conditions = .false. ! set to .true. to generate new initial conditions
+    logical            :: new_initial_conditions = .true. ! set to .true. to generate new initial conditions
     logical            :: verbose = .false.
     logical            :: save_results = .true. ! set to .false. to disable saving results (for bulk runs)
 
