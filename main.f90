f1c9f882a1d5abd5ed4a43eadc505fe3527d4e34
Leirof <vince.lrf@gmail.com>
2022-12-04 07:33:05 +0100

    Dev in 20min at 7am 😎

--- main.f90
+++ main.f90
@@ -2,178 +2,233 @@ program nbody
 
     implicit none
 
+    !----------------------------------------------------------------------------------------------------
+    ! Config
+
     ! Safe to edit
     integer, parameter :: N = 1000 ! number of bodies
-    integer, parameter :: steps = 100 ! simulation time in steps
+    integer, parameter :: steps = 1000 ! simulation time in steps
     real, parameter    :: dt = 0.1 ! time step in seconds
-    logical            :: new_initial_conditions = .true. ! set to .true. to generate new initial conditions
-    logical            :: verbose = .false.
-    logical            :: save_results = .true. ! set to .false. to disable saving results (for bulk runs)
+    ! logical            :: use_initial_conditions = .false. ! set to .true. to generate new initial conditions
+    ! logical            :: verbose = .false.
+    ! logical            :: save_results = .true. ! set to .false. to disable saving results (for bulk runs)
+
+    !----------------------------------------------------------------------------------------------------
+    ! Other declarations
 
     ! Don't touch to that, you fool!
-    ! integer                    :: MAX_TENTATIVES = 1000
-    ! INTEGER                    :: tentatives = 0
-    INTEGER                    :: i, t, tmp = -1
-    REAL                       :: PI
-    REAL                       :: x, y, z
-    real, dimension(3,N,steps) :: p=0, v=0, a=0
-    real                       :: m
-    real, dimension(steps)     :: Ep=0, Ec=0, Et=0
-    real, dimension(3,steps)   :: b ! barycenter
+    integer                    :: i, t
+    real                       :: PI
+    real, dimension(N)         :: x, y, z
+    real, dimension(N)         :: x2, y2, z2
+    real, dimension(N)         :: vx, vy, vz
+    real, dimension(N)         :: ax, ay, az
+    real, dimension(N)         :: ax2, ay2, az2
+    real                       :: Ep, Ec, Et
+    real                       :: bx, by, bz ! barycenter
+    logical                    :: lock
 
     PI = acos(-1.0)
 
-    ! print *, "NBody simulation..."
+    !----------------------------------------------------------------------------------------------------
+    ! Output files
 
-    !--------------------
-    ! Initial conditions
-    !--------------------
+    open(10, file='data/positions.txt', status='replace')
+    write(10, '(a)') '# x y z'
+    open(11, file='data/velocities.txt', status='replace')
+    write(11, '(a)') '# vx vy vz'
+    open(12, file='data/accelerations.txt', status='replace')
+    write(12, '(a)') '# ax ay az'
+    open(13, file='data/energies.txt', status='replace')
+    write(13, '(a)') '# Ep Ec Et'
+    open(14, file='data/barycenter.txt', status='replace')
+    write(14, '(a)') '# bx by bz'
+    open(15, file='data/parameters.txt', status='replace')
+    write(15, '(a)') '# N steps dt'
+    write(15, *) N, steps, dt
 
-    m = 1./N
+    !----------------------------------------------------------------------------------------------------
+    ! Initial positions
 
-    ! Position
-    if (new_initial_conditions .eqv. .false.) then
-        if (verbose .eqv. .true.) then
-            print *, "Getting initial conditions"
-        end if
-        open(42, file='data/initial_positions.txt', status='old')
-        do i= 1, N
-            read(42, *) p(:, i, 1)
-        end do
-        close(42)
-    
-    else
-        do i= 1, N
-
-            if (verbose .eqv. .true.) then
-                if (i * 100 / N .ne. tmp) then
-                    tmp = i * 100 / N
-                    print *, "Generating initial conditions ", tmp, "%"
-                end if
-            end if
-
-            ! generating random coordinate
-            call random_coord_in_sphere(x,y,z)
-
-            ! Storing the data
-            p(1, i, 1) = x
-            p(2, i, 1) = y
-            p(3, i, 1) = z
-
-        end do
-
-        if (verbose .eqv. .true.) then
-            print *, "Saving initial conditions"
-        end if
-
-        open(42, file='data/initial_positions.txt', status='replace')
-        do i= 1, N
-            write(42, *) p(:, i, 1)
-        end do
-        close(42)
-    end if
-
-    ! Velocities
-    
     do i=1,N
-        ! Highly complex computation of object velocity
-        ! Assumption to compute it: angular velocity around z axes with constant value = 1
-        v(1, i, 1) = -y
-        v(2, i, 1) = x
-        v(3, i, 1) = 0
-        
-    end do
+        lock = .true.
+        do while (lock .eqv. .true.)
+            CALL RANDOM_NUMBER(x(i))
+            x(i) = x(i) * 2.0 - 1.0
+            CALL RANDOM_NUMBER(y(i))
+            y(i) = y(i) * 2.0 - 1.0
+            CALL RANDOM_NUMBER(z(i))
+            z(i) = z(i) * 2.0 - 1.0
 
-    ! Accelerations
-
-    ! Computing initial acceleration
-    call acceleration(p(:, :, 1), m, N, a(:, :, 1), Ep(1))
-
-    ! Barycenter
-
-    call barycenter(p(:,:,1), m, N, b(:,1))
-
-    ! Energies
-    
-    ! do i=1,N
-    !     Ec(1) = Ec(1) + 0.5 * m * (v(1, i, 1) ** 2 + v(2, i, 1) ** 2 + v(3, i, 1) ** 2)
-    ! end do
-    ! Et(1) = Ep(1) + Ec(1)
-
-    !--------------------
-    ! Time evolution
-    !--------------------
-
-    do t=1,steps-1
-    
-        ! ! Print progress
-        if (verbose .eqv. .true.) then
-            if (t * 100 / steps .ne. tmp) then
-                tmp = t * 100 / steps
-                print *, "Computing evolution ", tmp, "%"
+            if (x(i)**2 + y(i)**2 + z(i)**2 < 1) then
+                lock = .false.
             end if
-        end if
+        end do
+    end do
 
-        ! Compute particle position, velocity and acceleration at time t+1
-        call next_state(p, v, a, t, m, dt, N, steps, Ep(t+1), Ec(t+1), Et(t+1))
-        call barycenter(p(:,:,t+1), m, N, b(:,t+1))
+    !----------------------------------------------------------------------------------------------------
+    ! Initial speeds
+
+    vx = -y
+    vy = x
+    vz = 0.0
+
+    !----------------------------------------------------------------------------------------------------
+    ! Initial accelerations
+
+    call acceleration(x, y, z, N, ax, ay, az, Ep)
+
+    !----------------------------------------------------------------------------------------------------
+    ! Initial energies
+
+    Ec = 0.0
+    do i=1,N
+        Ec = Ec + 0.5 * (vx(i)**2 + vy(i)**2 + vz(i)**2)
+    end do
+    Et = Ec + Ep
+
+    !----------------------------------------------------------------------------------------------------
+    ! Initial barycenter
+
+    bx = 0.0
+    by = 0.0
+    bz = 0.0
+    do i=1,N
+        bx = bx + x(i)
+        by = by + y(i)
+        bz = bz + z(i)
+    end do
+    bx = bx / N
+    by = by / N
+    bz = bz / N
+
+    !----------------------------------------------------------------------------------------------------
+    ! Main loop
+
+    do t=1,steps
+
+        !------------------------------
+        ! Position at t+1/2
+        do i=1,N
+            x2(i) = x(i) + vx(i) * dt/2. 
+            y2(i) = y(i) + vy(i) * dt/2.
+            z2(i) = z(i) + vz(i) * dt/2.
+        end do
+
+        !------------------------------
+        ! Acceleration at t+1/2
+        call acceleration(x2, y2, z2, N, ax2, ay2, az2)
+
+        !------------------------------
+        ! Compute velocity at t+1
+        do i=1,N
+            vx(i) = vx(i) + ax(i) * dt/2 + ax2(i) * dt/2
+            vy(i) = vy(i) + ay(i) * dt/2 + ay2(i) * dt/2
+            vz(i) = vz(i) + az(i) * dt/2 + az2(i) * dt/2
+        end do
+
+        !------------------------------
+        ! Position at t+1
+        do i=1,N
+            x(i) = x2(i) + vx(i) * dt/2
+            y(i) = y2(i) + vy(i) * dt/2
+            z(i) = z2(i) + vz(i) * dt/2
+        end do
+
+        !------------------------------
+        ! Acceleration at i+1
+        call acceleration(x, y, z, N, ax, ay, az, Ep)
+
+        !------------------------------
+        ! Energy at t+1
+        Ec = 0.0
+        do i=1,N
+            Ec = Ec + 0.5 * (vx(i)**2 + vy(i)**2 + vz(i)**2)
+        end do
+        Et = Ec + Ep
+
+        !------------------------------
+        ! Barycenter
+        bx = 0.0
+        by = 0.0
+        bz = 0.0
+        do i=1,N
+            bx = bx + x(i)
+            by = by + y(i)
+            bz = bz + z(i)
+        end do
+        bx = bx / N
+        by = by / N
+        bz = bz / N
+
+        !------------------------------
+        ! Print results
+
+        write(10, *) x, y, z
+        write(11, *) vx, vy, vz
+        write(12, *) ax, ay, az
+        write(13, *) Ep, Ec, Et
+        write(14, *) bx, by, bz
 
     end do
 
-    !--------------------
-    ! Export results
-    !--------------------
+    !----------------------------------------------------------------------------------------------------
+    ! Close files
 
-    if (save_results .eqv. .true.) then
+    close(10)
+    close(11)
+    close(12)
+    close(13)
+    close(14)
+    close(15)
 
-        ! Writing bodies position, velocity and acceleration
-        if (verbose .eqv. .true.) then
-            print *, "Saving bodies data"
-        end if
-        open(42, file='data/nbody.txt', status='replace')
-        write(42, *) "# p_x     p_y     p_z     v_x     v_y     v_z     a_x     a_y     a_z"
-        do t = 1, steps
-            write(42, *) " "
-            write(42, *) "# t = ", t*dt, " step = ", t
-            do i = 1, N
-                write(42, *) p(:,i,t), v(:,i,t), a(:,i,t)
+    !====================================================================================================
+    ! Subroutines & Functions
+    !====================================================================================================
+
+    contains
+
+        !----------------------------------------------------------------------------------------------------   
+        ! Compute accelerations
+
+        subroutine acceleration(x, y, z, N, ax, ay, az, Ep)
+            real,    intent(in   ), dimension(N) :: x, y, z
+            real,    intent(out  ), dimension(N) :: ax, ay, az
+            real,    intent(out  ), optional     :: Ep
+            integer, intent(in   )               :: N
+            real                                 :: m, G=1.0, eps=0.05
+            real                                 :: dx, dy, dz, r
+            integer                              :: i, j
+
+            ax = 0
+            ay = 0
+            az = 0
+            Ep = 0
+            m = 1.0/N
+
+            ! omp parallel reduction(+:Ep)
+            ! omp do
+            do i=1,N
+                do j=1,N
+                    if (j==i) then
+                        cycle
+                    end if
+                    
+                    dx = x(j) - x(i)
+                    dy = y(j) - y(i)
+                    dz = z(j) - z(i)
+
+                    r = sqrt(dx*dx + dy*dy + dz*dz + eps*eps)
+
+                    ax = ax + dx * G * m / (r**3)
+
+                    Ep = ep - 0.5*G*m**2/r
+                end do
             end do
-        end do
-        close(42)
-        
-        ! Writing system energies
-        if (verbose .eqv. .true.) then
-            print *, "Saving energies"
-        end if
-        open(42, file='data/energies.txt', status='replace')
-        write(42, *) "# t     Ep     Ec     Et"
-        do t = 1, steps
-            write(42, *) t*dt, Ep(t), Ec(t), Et(t)
-        end do
-        close(42)
+            ! omp parallel
+            ! omp do
 
-        ! Writing barycenter
-        if (verbose .eqv. .true.) then
-            print *, "Saving energies"
-        end if
-        open(42, file='data/barycenter.txt', status='replace')
-        write(42, *) "# t, barycenter"
-        do t = 1, steps
-            write(42, *) t*dt, b(:,t)
-        end do
-        close(42)
-        
-        ! Writing simulationn parameters
-        if (verbose .eqv. .true.) then
-            print *, "Saving simulation parameters"
-        end if
-        open(42, file='data/parameters.txt', status='replace')
-        write(42, *) "# N     steps     dt"
-        write(42, *) N, steps, dt
-        close(42)
-    
-    end if
+        end subroutine acceleration
 
-    ! print *, "Done!"
 
 end program nbody
--- src/compile.sh
+++ src/compile.sh
@@ -1,2 +1,2 @@
 export OMP_NUM_THREADS=$(nproc --all)
-gfortran -O3 -fopenmp -Wall -fbounds-check ./subroutines.f90 ./main.f90 -o ./data/main.out
+gfortran -O3 -fopenmp -Wall -fbounds-check ./main.f90 -o ./data/main.out
